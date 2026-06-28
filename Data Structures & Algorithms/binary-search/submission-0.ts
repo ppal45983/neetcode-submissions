ab2a68a55bc114ed9a7131173cd02e07860aef46
ppal45983 <90701005+ppal45983@users.noreply.github.com>
2026-06-28 12:43:13 +0530

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.ts
@@ -0,0 +1,12 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums: number[], target: number): number {
+        let index = nums.indexOf(target);
+        return index;
+
+    }
+}
