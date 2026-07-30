41d96a0ffb24f5430d4d86b42bb2e728dcc81f14
ppal45983 <90701005+ppal45983@users.noreply.github.com>
2026-07-30 18:57:38 +0530

    Add: reverse-string - submission-0

---
+++ Data Structures & Algorithms/reverse-string/submission-0.ts
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {character[]} s
+     * @return {void} Do not return anything, modify s in-place instead.
+     */
+    reverseString(s: string[]): void {
+        let start = 0;
+        let end = s.length - 1;
+        while (start < end) {
+            let temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+            start++;
+            end--;
+        }
+        return;
+    }
+}
